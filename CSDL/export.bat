62298634d0be83565f6a936d4b02cefeaf56b2fd
hoang_ngoc <vuhoangngoc07072004@gmail.com>
2025-05-06 11:53:11 +0700

    Cập nhật config.php, core/* và file cac file CSDL

--- CSDL/export.bat
+++
@@ -1,2 +0,0 @@
-@REM Remeber to run import before export
-mysqldump -u admin -padmin hotel > database.sql
--- CSDL/import.bat
+++
@@ -1,2 +0,0 @@
-@REM Remember to run import before export
-mysql -u admin -padmin -h localhost hotel < database.sql
